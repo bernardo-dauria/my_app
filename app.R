5aca5d1e7e02e317e3b887770d2a7af1de2bed7d
jupyter <jupyter@minivideos.uc3m.es>
2019-03-06 10:37:04 +0100

    server sends list_choices

--- app.R
+++ app.R
@@ -22,23 +22,30 @@ ui <- fluidPage(
    includeMarkdown("references.md"),
    
    selectInput("select", label = h3("Plot by type of alimentation"), 
-               choices = list_choices,
+               choices = character(0),
                selected = 1),
    
    plotOutput(outputId = "plot")
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  updateSelectInput(session, "select",
+                    choices = list_choices,
+                    selected = tail(list_choices, 1)
+  )
   
   col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
   
   output$plot <- renderPlot({
-    ggplot(msleep %>% filter(vore == input$select), 
-           aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +
-      col_scale +
-      geom_point()
+    if(input$select != ""){
+      ggplot(msleep %>% filter(vore == input$select), 
+             aes(bodywt, sleep_total, colour = vore)) +
+        scale_x_log10() +
+        col_scale +
+        geom_point()
+    }
   })
 }
 
