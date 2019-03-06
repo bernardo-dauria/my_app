af30e8a6528498f6f71a774109d37734cbbfb4d8
jupyter <jupyter@minivideos.uc3m.es>
2019-03-06 11:53:00 +0100

    enable and disable slider

--- app.R
+++ app.R
@@ -8,30 +8,31 @@
 #
 # Additional Bootstrap themes at [Bootswatch](http://bootswatch.com/)
 
-library(shiny)
-library(tidyverse)
+require(shiny)
+require(tidyverse)
+require("shinyjs")
 
 list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
 names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Shiny app",
-                 
                  tabPanel("msleep",
                           fluidPage( 
-                            sidebarLayout(# position = "right",
+                            sidebarLayout(
                               sidebarPanel(
                                 selectInput("select", label = h3("Plot by type of alimentation"), 
                                             choices = character(0),
                                             selected = 1)
-                              ),
+                              ), # sidebarPanel
                               mainPanel(
                                 plotOutput(outputId = "plot")
-                              )
-                            )
+                              ) # mainPanel
+                            ) # sidebarLayout
                           ) # fluidPage
-                 ), #  titlePanel
+                 ), #  tabPanel
                  tabPanel("Random generator",
+                          useShinyjs(),
                           sidebarLayout(position = "right",
                                         sidebarPanel(
                                           selectInput("dist", label = h3("Select the distribution"), 
@@ -39,8 +40,15 @@ ui <- navbarPage("Shiny app",
                                                       selected = 1),
                                           sliderInput("n_sample", label = h3("Number of samples"), min = 10, 
                                                       max = 100, value = 50),
-                                          sliderInput("n_bins", label = h3("Number of bins"), min = 1, 
-                                                      max = 50, value = 30)
+                                          fluidRow(
+                                            h3(style = "margin-left: 20px; margin-bottom: 0px;", "Number of bins"),
+                                            column(2,
+                                                   div(style = "margin-top: 37px", checkboxInput("auto_bins", label = "auto", value = TRUE))
+                                            ),
+                                            column(10,
+                                                   sliderInput("n_bins", label="", min = 1, max = 50, value = 30)
+                                            )
+                                          )
                                         ), # sidebarPanel
                                         mainPanel(
                                           plotOutput(outputId = "pulpo")
@@ -49,7 +57,7 @@ ui <- navbarPage("Shiny app",
                  ), #  tabPanel
                  tabPanel("References",
                           includeMarkdown("references.md")
-                 ) #  titlePanel
+                 ) #  tabPanel
 ) # navbarPage
 
 col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
@@ -61,7 +69,7 @@ server <- function(input, output, session) {
   updateSelectInput(session, "select",
                     choices = list_choices,
                     selected = tail(list_choices, 1)
-  )
+  );
   
   output$plot <- renderPlot({
     # cat(file=stderr(), "input$select:", input$select == "", "\n")
@@ -71,11 +79,17 @@ server <- function(input, output, session) {
         col_scale +
         geom_point()
     }
-  })
+  });
   
   cmd = reactive(eval(parse(text=paste(input$dist,"(",input$n_sample,")",sep=""))));
   
-  output$pulpo <- renderPlot(hist(cmd(), breaks=input$n_bins));
+  observe(if(input$auto_bins) disable("n_bins") else enable("n_bins") )
+  
+  output$pulpo <- renderPlot(
+    if(input$auto_bins) hist(cmd()) 
+    else hist(cmd(), breaks=input$n_bins)
+  );
+  
 }
 
 # Run the application 
